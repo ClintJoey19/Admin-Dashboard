ddbd09faac7dfe9e2815cd256a1692bf0485f24b
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-03-18 21:31:31 +0800

    sidebar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
-import { useState } from "react";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Layout from "./pages/layout";
 
 function App() {
-  return <div className="app">Hello</div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,193 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
+} from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import profileImage from "../assets/profile.jpg";
+
+const navItems = [
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Client Facing",
+    icon: null,
+  },
+  {
+    text: "Products",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: "Customers",
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: "Transactions",
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: "Geography",
+    icon: <PublicOutlined />,
+  },
+  {
+    text: "Sales",
+    icon: null,
+  },
+  {
+    text: "Overview",
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: "Daily",
+    icon: <TodayOutlined />,
+  },
+  {
+    text: "Monthly",
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: "Breakdown",
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+  {
+    text: "Admin",
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: "Performance",
+    icon: <TrendingUpOutlined />,
+  },
+];
+
+const Sidebar = ({
+  isNonMobile,
+  drawerWidth,
+  isSideBarOpen,
+  setIsSideBarOpen,
+}) => {
+  const { pathname } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
+
+  return (
+    <Box component="nav">
+      {isSideBarOpen && (
+        <Drawer
+          open={isSideBarOpen}
+          onClose={() => setIsSideBarOpen(false)}
+          variant="persistent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            "& .MuiDrawer-paper": {
+              color: theme.palette.secondary[200],
+              backgroundColor: theme.palette.background.alt,
+              boxSizing: "border-box",
+              borderWidth: isNonMobile ? 0 : "2px",
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Box width="100%">
+            <Box m="1.5 rem 2rem 2rem 3rem">
+              <FlexBetween color={theme.palette.secondary.main}>
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    ECOMVISION
+                  </Typography>
+                </Box>
+                {!isNonMobile && (
+                  <IconButton onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+                    <ChevronLeft />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            </Box>
+            <List>
+              {navItems.map(({ text, icon }, i) =>
+                !icon ? (
+                  <Typography key={i} m="2.5rem 0 1rem 3rem">
+                    {text}
+                  </Typography>
+                ) : (
+                  <ListItemButton
+                    key={i}
+                    onClick={() => {
+                      navigate(`${text.toLocaleLowerCase()}`);
+                      setActive(text.toLocaleLowerCase());
+                    }}
+                    sx={{
+                      backgroundColor:
+                        active === text.toLocaleLowerCase()
+                          ? theme.palette.secondary[300]
+                          : "transparent",
+                      color:
+                        active === text.toLocaleLowerCase()
+                          ? theme.palette.primary[600]
+                          : theme.palette.secondary[200],
+                    }}
+                  >
+                    <ListItemIcon
+                      sx={{
+                        ml: "2rem",
+                        color:
+                          active === text.toLocaleLowerCase()
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[200],
+                      }}
+                    >
+                      {icon}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                    {active === text.toLocaleLowerCase() && (
+                      <ChevronRightOutlined sx={{ ml: "auto" }} />
+                    )}
+                  </ListItemButton>
+                )
+              )}
+            </List>
+          </Box>
+        </Drawer>
+      )}
+    </Box>
+  );
+};
+
+export default Sidebar;
